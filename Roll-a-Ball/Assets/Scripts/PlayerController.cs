59c58657e3f619a6890214e9a11fa2acab093eba
benjamin-wss <benjamin.wss@gmail.com>
2015-05-02 22:07:58 +0800

    Movement Script Added
    1. Added a simple script that will add movement and physics handling to the player actor.

---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rigidbody.AddForce(movement * Speed);
+    }
+}
