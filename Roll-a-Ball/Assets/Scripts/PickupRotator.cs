b1e6c1179dc6205a1281a94492c1f473e5cf993c
benjamin-wss <benjamin.wss@gmail.com>
2015-05-04 00:26:53 +0800

    Pickup Object Defined
    1. Defined Prefab for pickups
    
    2. Added pickups to scene
    
    3. Created rotation to mmake pickups stand out
    
    4. Created pickup material to give unique color

---
+++ Roll-a-Ball/Assets/Scripts/PickupRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PickupRotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var pickupTransformRotation = new Vector3(15, 30, 45);
+
+        transform.Rotate(pickupTransformRotation * Time.deltaTime);
+    }
+}
