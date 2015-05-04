edad04f5207ed3fb1c3c91a2acd30621a7ee0866
benjamin-wss <benjamin.wss@gmail.com>
2015-05-04 18:19:11 +0800

    Setup Pickup Mechanism
    1. Pickup tag is setup so player object knows that to look for.
    
    2. Colission script to hide objects on contact is setup.
    
    3. Pickups now have a ridig body component and is kenmatic to optimize performance.

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,5 +21,15 @@ public class PlayerController : MonoBehaviour
         var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         _rigidbody.AddForce(movement * Speed);
+    }
+
+    void OnTriggerEnter([NotNull] Collider other)
+    {
+        if (other == null) throw new ArgumentNullException("other");
+
+        if (!string.IsNullOrEmpty(other.gameObject.tag) && other.gameObject.tag == "Pick_Up")
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
