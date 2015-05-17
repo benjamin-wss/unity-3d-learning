f5eab6c258564a7893ef4ffd18d1773268ce633f
benjamin-wss <benjamin.wss@gmail.com>
2015-05-17 23:35:49 +0800

    Defined asteroid prefab
    1. Added explosion both both asteroid and player when player collides with prefab.
    2. Created prefab for asteroud for reuse.

--- Space-Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space-Shooter/Assets/Scripts/DestroyByContact.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject Explosion;
+    public GameObject PlayerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         var currentColisionItemTag = other.tag;
         
         if(!string.IsNullOrEmpty(currentColisionItemTag) && !string.Equals(currentColisionItemTag, "Boundary", StringComparison.OrdinalIgnoreCase))
         {
+            Instantiate(Explosion, transform.position, transform.rotation);
             Destroy(other.gameObject);
             Destroy(gameObject);
-        } 
+
+            if (string.Equals(currentColisionItemTag, "Player", StringComparison.OrdinalIgnoreCase))
+            {
+                Instantiate(PlayerExplosion, transform.position, transform.rotation);
+            }
+        }
     }
 }
