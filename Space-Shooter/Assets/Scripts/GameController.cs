18ffd0ac7de5df5d1918bbe28aef3c5b49c5bf99
benjamin-wss <benjamin.wss@gmail.com>
2015-05-17 23:51:49 +0800

    Defined game controller
    1. Defined game controller to spawn a randomized asteroid. More work needed.

---
+++ Space-Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Hazzard;
+    public Vector3 BaseSpawnPosition;
+
+    void Start()
+    {
+        var randomizedSpawnPosition = new Vector3(Random.Range(-BaseSpawnPosition.x, BaseSpawnPosition.x), BaseSpawnPosition.y, BaseSpawnPosition.z);
+        var spawnRotation = Quaternion.identity;
+        SpawnWaves(Hazzard, randomizedSpawnPosition, spawnRotation);
+    }
+
+    private static void SpawnWaves(GameObject hazzard, Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        Instantiate(hazzard, spawnPosition, spawnRotation);
+    }
+}
