1a30b0b02f6b7cdb5b342a8f6f38b4b74ef1fab7
benjamin-wss <benjamin.wss@gmail.com>
2015-05-19 00:14:14 +0800

    Added Game Scoring System.

--- Space-Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space-Shooter/Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,23 @@ public class DestroyByContact : MonoBehaviour
     public GameObject Explosion;
     public GameObject PlayerExplosion;
 
+    private GameController _targetGameController;
+    public int HazardKillScore;
+
+    void Start()
+    {
+        var gameController = GameObject.FindGameObjectWithTag("GameController");
+
+        if (gameController != null)
+        {
+            _targetGameController = gameController.GetComponent<GameController>();
+        }
+        else
+        {
+            Debug.Log("Unable to find 'GameController' object script");
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         var currentColisionItemTag = other.tag;
@@ -13,8 +30,10 @@ public class DestroyByContact : MonoBehaviour
         if(!string.IsNullOrEmpty(currentColisionItemTag) && !string.Equals(currentColisionItemTag, "Boundary", StringComparison.OrdinalIgnoreCase))
         {
             Instantiate(Explosion, transform.position, transform.rotation);
+
             Destroy(other.gameObject);
             Destroy(gameObject);
+            _targetGameController.AddScore(HazardKillScore);
 
             if (string.Equals(currentColisionItemTag, "Player", StringComparison.OrdinalIgnoreCase))
             {
--- Space-Shooter/Assets/Scripts/GameController.cs
+++ Space-Shooter/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
@@ -13,8 +14,13 @@ public class GameController : MonoBehaviour
     public float StartWait;
     public float WaveWait;
 
+    public Text ScoreText;
+    private int _score;
+
     void Start()
     {
+        _score = 0;
+        UpdateScore();
         StartCoroutine(SpawnWaves(Hazard, BaseSpawnPosition, HazardCount, SpawnWait, StartWait, WaveWait));
     }
 
@@ -48,5 +54,18 @@ public class GameController : MonoBehaviour
             yield return new WaitForSeconds(waveWait);
         }
         
+    }
+
+    public void AddScore(int kill)
+    {
+        _score += kill;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        const string scoreTextTemplate = "Score: {0}";
+
+        ScoreText.text = string.Format(scoreTextTemplate, _score);
     }
 }
